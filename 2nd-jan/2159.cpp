320c53db2039aa22cda6cdb5e327eba5d4cbd0e2
Anton Jaehope <kojotourist@gmail.com>
2021-01-12 07:19:17 +0900

    :whale: 케익배달 수정

--- 2nd-jan/2159.cpp
+++ 2nd-jan/2159.cpp
@@ -4,53 +4,42 @@
 #include <climits>
 
 using namespace std;
-int N, X, Y;
-int answer = INT_MAX;
-const int MAX = 100000;
-int direction[5]{1, -1, 0, 0, 0};
 
-void dp(pair<int, int> cur, vector<pair<int, int>>& v, int index, int sum) {
-  if(v.size() == index) {
-    answer = min(answer, sum);
-    return; // end of the dp
-  }
+int main() {
+  int N, X, Y;
+  long long answer = LLONG_MAX;
+  int direction[5]{1, -1, 0, 0, 0};
+  long long dp[2][5];
 
-  map<pair<int,int>, int> m;
-  pair<int, int> next = v[index]; //next node
-  int minimum{1 << 13};
-  for(int i=0; i<5; i++){
-    pair<int, int> temp{next.first+direction[i], next.second+direction[4-i]};
-    if(temp.first > MAX || temp.first < 0 || temp.second > MAX || temp.second < 0) continue; // check possibility
-    int distance = abs(temp.first-cur.first) + abs(temp.second-cur.second);
-    minimum = min(distance, minimum);
-    m[temp] = distance;
-  }
-
-  for (auto const& e : m){
-    if(e.second == minimum) {
-      dp(e.first, v, index+1, sum+e.second);
+  cin >> N >> X >> Y;
+  for(int i=0; i<2; i++){
+    for(int j=0; j<5; j++){
+      if(i==0) dp[i][j] = abs(direction[j] + direction[4-j]);
+      else dp[i][j] = LLONG_MAX;
     }
   }
 
-  return;
-}
-
-int main() {
-  cin >> N >> X >> Y;
-  vector<pair<int, int>> v;
-
-  for(int i=0; i<N; i++) {
+  for(int i=1; i<=N; i++) {
     int x, y;
     cin >> x >> y;
-    v.push_back({x, y});
+
+    for(int j=0; j<5; j++) { // prev
+      for(int k=0; k<5; k++) { // next
+        long long distance = abs(X+direction[j]-(x+direction[k])) + abs(Y+direction[4-j]-(y+direction[4-k]));
+        dp[i%2][k] = min(dp[i%2][k], dp[(i+1)%2][j] + distance);
+      }
+    }
+
+    for(int j=0; j<5; j++) dp[(i+1)%2][j] = LLONG_MAX;
+
+    X = x; Y= y;
   }
 
-  dp({X, Y}, v, 0, 0);
-
-  if(answer == INT_MAX) {
-    cout << 0;
-  } else {
-    cout << answer;
+  for(int i=0; i<5; i++) {
+    answer = min(answer, dp[N%2][i]);
   }
+
+  cout << answer;
+
   return 0;
 }
