b4cd21664d16f09213c24f6abeec30ab5326fcf2
이재호 <kojotourist@gmail.com>
2021-12-16 18:46:06 +0900

    숫자 재배치

---
+++ 2021/nov/16943.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int A, B;
+    std::cin >> A >> B;
+    std::string sta = std::to_string(A);
+    std::string start;
+
+    while (std::prev_permutation(sta.begin(), sta.end()))
+    {
+        start = sta;
+    }
+
+    int result = -1;
+    do
+    {
+        int temp = std::stoi(start);
+        if (temp < B && start[0] != '0')
+            result = std::max(result, temp);
+    } while (std::next_permutation(start.begin(), start.end()));
+
+    std::cout << result;
+
+    return 0;
+}
