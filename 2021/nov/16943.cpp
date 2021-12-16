ca15e0a942e811e559c21c2ce4f43097666f9722
이재호 <kojotourist@gmail.com>
2021-12-16 19:59:21 +0900

    숫숫자  재재배배치

--- 2021/nov/16943.cpp
+++ 2021/nov/16943.cpp
@@ -7,7 +7,7 @@ int main()
     int A, B;
     std::cin >> A >> B;
     std::string sta = std::to_string(A);
-    std::string start;
+    std::string start = sta;
 
     while (std::prev_permutation(sta.begin(), sta.end()))
     {
@@ -17,6 +17,7 @@ int main()
     int result = -1;
     do
     {
+        // stoi
         int temp = std::stoi(start);
         if (temp < B && start[0] != '0')
             result = std::max(result, temp);
