230342de84f495d4ba6ee4f00e2831de0e0685bd
이재호 <kojotourist@gmail.com>
2022-09-06 08:39:35 +0900

    add_two_numbers

---
+++ rust/add_two_numbers/src/main.rs
@@ -0,0 +1,30 @@
+/*
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+ */
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    
+}
+
+fn main() {
+    println!("Hello, world!");
+}
